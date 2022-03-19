abab26da997bd31aa924138542d7ff8792dc8120
matsufuji06 <n.matsufuji06@gmail.com>
2022-03-20 08:18:06 +0900

    date_default_timezone_setで日本時間にして時刻を表示

--- lesson01/sample04.php
+++ lesson01/sample04.php
@@ -4,11 +4,12 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=], initial-scale=1.0">
-  <title>sample02</title>
+  <title>sample04</title>
 </head>
 <body>
   <?php
-  echo 'I\'m studying';
+  date_default_timezone_set('Asia/Tokyo');
+    echo date('G時 i分 s秒');
   ?>
 </body>
 </html>
