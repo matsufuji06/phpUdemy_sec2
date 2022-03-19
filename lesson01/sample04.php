08020df230312acfbb9abf096e188cab18b6607e
matsufuji06 <n.matsufuji06@gmail.com>
2022-03-20 08:38:38 +0900

    htmlと混在させて変数を表示

--- lesson01/sample04.php
+++ lesson01/sample04.php
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=], initial-scale=1.0">
-  <title>sample04</title>
-</head>
-<body>
-  <?php
+<?php
   date_default_timezone_set('Asia/Tokyo');
   echo '現在は ' . date('G時 i分 s秒') . 'です。';
   echo '現在は ', date('G時 i分 s秒'), 'です。';
-  ?>
-</body>
-</html>
+?>
