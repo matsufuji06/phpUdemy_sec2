101438bba0fd1ea3484633ce95bf82ceebd95ce0
matsufuji06 <n.matsufuji06@gmail.com>
2022-03-21 11:20:24 +0900

    xmlからデータを取得し、配列形式で表示

---
+++ lesson01/sample17.php
@@ -0,0 +1,13 @@
+<?php
+$xmlTree = simplexml_load_file('rss.xml');
+foreach($xmlTree->channel->item as $item):
+?>
+・<a href="<?php echo $item->link; ?>"><?php echo $item->title; ?></a><br>
+<?php
+endforeach;
+?>
+
+
+<?php 
+// echo $xmlTree->channel->item[1]->title; 
+?>
