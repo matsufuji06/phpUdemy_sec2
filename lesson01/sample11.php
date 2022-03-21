192c1e12e97dc87bd0d5ddc6e0528bcdb7809d68
matsufuji06 <n.matsufuji06@gmail.com>
2022-03-21 09:56:56 +0900

    連想配列の練習

---
+++ lesson01/sample11.php
@@ -0,0 +1,19 @@
+<?php
+$fruits = [
+  'apple' => 'りんご', 
+  'grape' => 'ぶどう',
+  'lemon' => 'レモン',
+  'tomato' => 'トマト',
+  'peach' => 'もも'
+
+];
+// echo $fruits['lemon'];
+?>
+
+<dl>
+  <?php foreach($fruits as $english => $japanese): ?>
+    <dt><?php echo $english; ?></dt>
+    <dd><?php echo $japanese; ?></dd>
+  <?php endforeach; ?>
+
+</dl>
