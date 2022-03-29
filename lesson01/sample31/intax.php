85d047b7a0726632263c270ad316853240737027
matsufuji06 <n.matsufuji06@gmail.com>
2022-03-29 10:15:15 +0900

    他のファイルに分けて関数を実行

---
+++ lesson01/sample31/intax.php
@@ -0,0 +1,8 @@
+<?php
+// 税込金額を返す
+function intax($value) {
+  return ceil($value * 1.1);
+
+}
+
+?>
---
+++ lesson01/sample31/page01.php
@@ -0,0 +1,7 @@
+<?php
+require('intax.php');
+
+$price = 200;
+$price_tax = intax($price);
+echo $price_tax;
+?>
